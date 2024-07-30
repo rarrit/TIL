90ecd3cdab3e0c526de3f3000cda1142438d3c28
김민규 <tjfvndfud@gmail.com>
2024-07-30 20:36:22 +0900

    JavaScript의 단축 속성명 STUDY

--- JavaScript/12. 단축 속성명/js.js
+++ JavaScript/12. 단축 속성명/js.js
@@ -5,7 +5,7 @@ const newAge = "30";
 // key - value 가 같으면 생략 가능하다.
 const obj = {
   name, // 변수명과 value값이 같으면 단축할 수 잇다. 
-  age : newAge,
+  age: newAge,
 }
 
 // 전개 구문 = spread operator
@@ -21,7 +21,7 @@ console.log(newArr); // [1, 2, 3, 4]
 // 객체에서의 전개 구문
 let user = {
   name: 'nbc',
-  age : 30,
+  age: 30,
 }
 
 let user2 = { ...user }
@@ -29,17 +29,18 @@ console.log(user); // { name: 'nbc', age : 30,}
 console.log(user2); // { name: 'nbc', age : 30,}
 
 // 나머지 매개변수 (Rest parameter)
-function exampleFunc (a, b, c, ...args) { // args : 추가적으로 들어올 수 있음
+function exampleFunc(a, b, c, ...args) { // args : 추가적으로 들어올 수 있음
   console.log(a, b, c);
   console.log(...args);
 }
-exampleFunc(1, 2, 3 , 4, 5, 6, 7); // 1, 2, 3 [4, 5, 6, 7]
+exampleFunc(1, 2, 3, 4, 5, 6, 7); // 1, 2, 3 [4, 5, 6, 7]
 
 // 탬플릿 리터럴(Template Literal)
 // 멀티라인을 지원함, '', ""은 한 줄로만 표현해야함
+// 자바스크립트 코드가 들어갈 수 있음
 const testValue = "안녕하세요.";
 console.log(`
   Hello World !!! 
   ${testValue} :)
-`); // 자바스크립트 코드가 들어갈 수 있음
+`);
 
