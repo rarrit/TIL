52635a423a4555a7375cfac0b32b304b21337d1e
김민규 <tjfvndfud@gmail.com>
2024-08-19 17:23:31 +0900

    react-app-without-cra 삭제

--- Project/react-app-without-cra/src/App.js
+++
@@ -1,9 +0,0 @@
-const apiUrl = process.env.APP_API_URL;
-
-console.log("API URL:", apiUrl);
-
-const App = () => {
-  return <h1>Hello React</h1>;
-};
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
