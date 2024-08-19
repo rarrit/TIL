4f6a87fd4d537f69208b4110e8197e186d5372c1
김민규 <tjfvndfud@gmail.com>
2024-08-19 16:21:44 +0900

    리액트 CRA,Vite 없이 React 개발 환경 구축하기

--- Project/react-app-without-cra/src/App.js
+++ Project/react-app-without-cra/src/App.js
@@ -1,5 +1,9 @@
+const apiUrl = process.env.APP_API_URL;
+
+console.log("API URL:", apiUrl);
+
 const App = () => {
-  return <h1>Hello, World Wow!</h1>;
+  return <h1>Hello React</h1>;
 };
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
